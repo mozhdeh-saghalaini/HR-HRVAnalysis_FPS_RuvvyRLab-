6460bd81031cabf88a7166803b69cea9058e125a
Mozhdeh Saghalaini <m.saghalaini@gmail.com>
2025-11-10 16:48:00 +0330

    adding things

--- Scripts/data_wrangling.R
+++ Scripts/data_wrangling.R
@@ -34,50 +34,17 @@ read_mindware_files <- function(filename) {
     collection_date <- participant_data[6] %>% lubridate::mdy() # such as "09072021" and then to "2021-09-07"
 
     # Creating a data frame with the metrics
-    metrics_df <- data.frame(
-      # Time and Basic Metrics
-      time = t(as.vector(d[36, 2:ncol(d)])) %>% as.numeric(),
-      epoch = 1:length(t(as.vector(d[36, 2:ncol(d)]))),
+    metrics <- data.frame(
       
-      # Heart Rate Metrics
-      hr = t(as.vector(d[37, 2:ncol(d)])) %>% as.numeric(),
-      mean_hr = t(as.vector(d[37, 2:ncol(d)])) %>% as.numeric(),
+      # time serie data
+      time_data <- t(as.vector(d["TimeElapsed Time row", 2:ncol(d)])) %>% as.numeric(),
+      hr_data <- t(as.vector(d["HR row", 2:ncol(d)])) %>% as.numeric(),
+      rmssd_data <- t(as.vector(d["RMSSD row", 2:ncol(d)])) %>% as.numeric(),
+      sdnn_data <- t(as.vector(d["SDNN row", 2:ncol(d)])) %>% as.numeric(),
+      rsa_data <- t(as.vector(d["RSA row", 2:ncol(d)])) %>% as.numeric(),
+      ibi_data <- t(as.vector(d["IBI row", 2:ncol(d)])) %>% as.numeric(),
+      pnn50_data <- t(as.vector(d["pNN50 row", 2:ncol(d)])) %>% as.numeric(),
       
-      # HRV Time-Domain Metrics
-      rmssd = t(as.vector(d[52, 2:ncol(d)])) %>% as.numeric(),
-      sdnn = t(as.vector(d[51, 2:ncol(d)])) %>% as.numeric(),
-      pnn50 = t(as.vector(d[53, 2:ncol(d)])) %>% as.numeric(),
-
-      # IBI and Interval Metrics
-      ibi = t(as.vector(d[39, 2:ncol(d)])) %>% as.numeric(),
-      mean_ibi = t(as.vector(d[39, 2:ncol(d)])) %>% as.numeric(),
-      
-      # Frequency Domain Metrics
-      hf_power = tryCatch({
-        t(as.vector(d[54, 2:ncol(d)])) %>% as.numeric()
-      }, error = function(e) rep(NA, length(t(as.vector(d[36, 2:ncol(d)]))))),
-      
-      lf_power = tryCatch({
-        t(as.vector(d[55, 2:ncol(d)])) %>% as.numeric()
-      }, error = function(e) rep(NA, length(t(as.vector(d[36, 2:ncol(d)]))))),
-      
-      vlf_power = tryCatch({
-        t(as.vector(d[56, 2:ncol(d)])) %>% as.numeric()
-      }, error = function(e) rep(NA, length(t(as.vector(d[36, 2:ncol(d)]))))),
-      
-      lf_hf_ratio = tryCatch({
-        t(as.vector(d[57, 2:ncol(d)])) %>% as.numeric()
-      }, error = function(e) rep(NA, length(t(as.vector(d[36, 2:ncol(d)]))))),
-      
-      # Data from the filename
-      id = id,
-      sex = sex,
-      task_type = task_type,
-      task_version = task_version,
-      collection_date = collection_date_parsed,
-      source_file = file_name,
-      
-      stringsAsFactors = FALSE
     )
     
     # Remove rows where time is NA (empty columns in original data)
